71473a4914b1e8ae1fa3e79a32c50d933fdc4e27
Fahim Chowdhury <fahimchowdhury950@gmail.com>
2020-12-10 16:41:43 +0600

    Google Map

---
+++ flutter_map/lib/main.dart
@@ -0,0 +1,70 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  GoogleMapController _controller;
+
+  final CameraPosition _initialPosition = CameraPosition(target: LatLng(23.777758, 90.405739));
+
+  final List<Marker> markers = [];
+
+  addMarker(cordinate) {
+    int id = Random().nextInt(100);
+
+    setState(() {
+      markers.add(Marker(position: cordinate, markerId: MarkerId(id.toString())));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: _initialPosition,
+        mapType: MapType.normal,
+        onMapCreated: (controller) {
+          setState(() {
+            _controller = controller;
+          });
+        },
+        markers: markers.toSet(),
+        onTap: (cordinate) {
+          _controller.animateCamera(CameraUpdate.newLatLng(cordinate));
+          addMarker(cordinate);
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _controller.animateCamera(CameraUpdate.zoomIn());
+        },
+        child: Icon(Icons.zoom_out),
+      ),
+    );
+  }
+}
